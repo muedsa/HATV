11b2bde642ace7ce18fd7a868bc4f867f69080e0
MUEDSA <7676275+muedsa@users.noreply.github.com>
2023-09-22 10:02:27 +0800

    update: send exoPlayer error to firebase

--- app/src/main/kotlin/com/muedsa/hatv/ui/features/playback/PlaybackScreen.kt
+++ app/src/main/kotlin/com/muedsa/hatv/ui/features/playback/PlaybackScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.remember
 import androidx.media3.common.MediaItem
 import androidx.media3.common.PlaybackException
 import androidx.media3.common.Player
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.muedsa.compose.tv.widget.ErrorMessageBox
 import com.muedsa.compose.tv.widget.ErrorMessageBoxState
 import com.muedsa.compose.tv.widget.player.SimpleVideoPlayer
@@ -20,6 +21,11 @@ fun PlaybackScreen(
             addListener(object : Player.Listener {
                 override fun onPlayerErrorChanged(error: PlaybackException?) {
                     errorMessageBoxState.error(error, SnackbarDuration.Long)
+                    error?.let {
+                        val crashlytics = FirebaseCrashlytics.getInstance()
+                        crashlytics.log("exoplayer mediaUrl: $mediaUrl")
+                        crashlytics.recordException(it)
+                    }
                 }
             })
             playWhenReady = true
