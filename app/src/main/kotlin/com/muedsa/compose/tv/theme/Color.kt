5c59cf1a52012ab10d6459a5dabcb0b78391949a
MUEDSA <7676275+muedsa@users.noreply.github.com>
2023-09-22 18:09:50 +0800

    update: color preview

--- app/src/main/kotlin/com/muedsa/compose/tv/theme/Color.kt
+++ app/src/main/kotlin/com/muedsa/compose/tv/theme/Color.kt
@@ -1,6 +1,7 @@
 package com.muedsa.compose.tv.theme
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,120 +16,159 @@ internal object CustomerColor {
     val outlineVariant = Color(red = 73, green = 69, blue = 79) // PaletteTokens.NeutralVariant30
 }
 
+
 @OptIn(ExperimentalTvMaterial3Api::class)
+@Composable
+fun TvColorPreview(modifier: Modifier = Modifier) {
+    TvLazyColumn(modifier) {
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.primary),
+                text = "[On]Primary",
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.primaryContainer),
+                text = "[On]PrimaryContainer",
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.inversePrimary),
+                text = "InversePrimary"
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.secondary),
+                text = "[On]Secondary",
+                color = MaterialTheme.colorScheme.onSecondary
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.secondaryContainer),
+                text = "[On]SecondaryContainer",
+                color = MaterialTheme.colorScheme.onSecondaryContainer
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.tertiary),
+                text = "[On]Tertiary",
+                color = MaterialTheme.colorScheme.onTertiary
+            )
+        }
+
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.background),
+                text = "[On]Background",
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.surface),
+                text = "[On]Surface",
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.surfaceVariant),
+                text = "[On]SurfaceVariant",
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.surfaceTint),
+                text = "SurfaceTint"
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.inverseSurface),
+                text = "[On]InverseSurface",
+                color = MaterialTheme.colorScheme.inverseOnSurface
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.error),
+                text = "[On]Error",
+                color = MaterialTheme.colorScheme.onError
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.errorContainer),
+                text = "[On]ErrorContainer",
+                color = MaterialTheme.colorScheme.onErrorContainer
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.border),
+                text = "Border",
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.borderVariant),
+                text = "BorderVariant"
+            )
+        }
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.scrim),
+                text = "Scrim"
+            )
+        }
+    }
+}
+
+
 @Preview
 @Composable
-fun ThemeColorPreview() {
+fun TvThemeColorPreview() {
     TvTheme {
-        TvLazyColumn {
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.primary),
-                    text = "primary",
-                    color = MaterialTheme.colorScheme.onPrimary
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer),
-                    text = "primaryContainer",
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.inversePrimary),
-                    text = "inversePrimary"
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.secondary),
-                    text = "secondary",
-                    color = MaterialTheme.colorScheme.onSecondary
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.secondaryContainer),
-                    text = "secondaryContainer",
-                    color = MaterialTheme.colorScheme.onSecondaryContainer
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.tertiary),
-                    text = "tertiary",
-                    color = MaterialTheme.colorScheme.onTertiary
-                )
-            }
-
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.background),
-                    text = "background",
-                    color = MaterialTheme.colorScheme.onBackground
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.surface),
-                    text = "surface",
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.surfaceVariant),
-                    text = "surfaceVariant",
-                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.surfaceTint),
-                    text = "surfaceTint"
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.inverseSurface),
-                    text = "inverseSurface",
-                    color = MaterialTheme.colorScheme.inverseOnSurface
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.error),
-                    text = "error",
-                    color = MaterialTheme.colorScheme.onError
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.errorContainer),
-                    text = "errorContainer",
-                    color = MaterialTheme.colorScheme.onErrorContainer
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.border),
-                    text = "border",
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.borderVariant),
-                    text = "borderVariant"
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.scrim),
-                    text = "scrim"
-                )
-            }
-        }
+        TvColorPreview()
     }
 }
